5fd8d8b81a9da184d245b9c670524a1e3d9d77c1
Zimeoo <102761596+Zimeoo@users.noreply.github.com>
2022-04-02 04:17:15 -0600

    Add files via upload

---
+++ Yes.java
@@ -0,0 +1,163 @@
+import java.util.Scanner;
+
+public class Yes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		String OW2 = ("Press 1 for adding");
+		String will = ("Press 2 for sub");
+		String be = ("Press 3 for divide");
+		String the = ("press 4 for multiplying");
+		String greatest = ("press 5 to goto main menu");	
+	
+		System.out.println( OW2);
+		System.out.println(will);
+		System.out.println(be);
+		System.out.println(the);
+		System.out.println(greatest);
+		
+		
+	
+		
+		
+		int x = scan.nextInt();
+		
+		if (x == 1 || x ==5) {
+	
+			System.out.println("you are now adding, enter your first number");
+			int fnum = scan.nextInt();
+			System.out.println("enter your second number");
+			int snum = scan.nextInt();
+			System.out.println("Answer");
+			System.out.println(fnum + snum);
+			System.out.println("press 5 if you would like to go back to main menu");
+			int back = scan.nextInt(); 
+			System.out.println( OW2);
+			System.out.println(will);
+			System.out.println(be);
+			System.out.println(the);
+			System.out.println(greatest);
+		
+		}	
+		 
+		else if (x == 2 || x ==5) {
+			
+			System.out.println("you are now subtracting, enter your first number");
+			int fnum = scan.nextInt();
+			System.out.println("enter your second number");
+			int snum = scan.nextInt();
+			System.out.println("Answer");
+			System.out.println(fnum - snum);
+			System.out.println("press 5 if you would like to go back to main menu");
+			int back = scan.nextInt(); 
+			System.out.println( OW2);
+			System.out.println(will);
+			System.out.println(be);
+			System.out.println(the);
+			System.out.println(greatest);
+		 
+		
+		
+		}
+		
+else if (x == 3 || x ==5) {
+			
+			System.out.println("you are now dividing, enter your first number");
+			int fnum = scan.nextInt();
+			System.out.println("enter your second number");
+			int snum = scan.nextInt();
+			System.out.println("Answer");
+			System.out.println(fnum / snum);
+			System.out.println("press 5 if you would like to go back to main menu");
+			int back = scan.nextInt(); 
+			System.out.println( OW2);
+			System.out.println(will);
+			System.out.println(be);
+			System.out.println(the);
+			System.out.println(greatest);
+		 
+		
+			}
+else if (x == 4 || x ==5) {
+	
+	System.out.println("you are now multiplying, enter your first number");
+	int fnum = scan.nextInt();
+	System.out.println("enter your second number");
+	int snum = scan.nextInt();
+	System.out.println("Answer");
+	System.out.println(fnum * snum);
+	System.out.println("press 5 if you would like to go back to main menu");
+	int back = scan.nextInt(); 
+	System.out.println( OW2);
+	System.out.println(will);
+	System.out.println(be);
+	System.out.println(the);
+	System.out.println(greatest);
+}
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+		
+
+		 
+		
+			
+			
+			
+			
+			
+			
+	
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		
+				
+			    
+				
+		
+		
+				
+		
+		
+		
+			
+		
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+		
+		
+	
+	
+
+
